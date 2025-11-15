9f49f22b79a7e03cbda89ffe0dc3a1d59aec1744
Sumanthtellapuri <telapurisumanth1234@gmail.com>
2025-11-15 15:21:18 +0530

    Fix PORT for Render

--- server.js
+++ server.js
@@ -86,5 +86,6 @@ app.post("/api/chat/:id", (req, res) => {
 // -----------------------------------------
 // Start Server
 // -----------------------------------------
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
